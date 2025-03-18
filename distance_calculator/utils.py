430175efc202148dc04530f2a5afc8ab32e780a9
Daniel <daniel.rubinst@gmail.com>
2025-03-18 15:11:46 +0200

    added requirements.txt

---
+++ distance_calculator/utils.py
@@ -0,0 +1,33 @@
+import sys
+import requests
+from pycountry import countries
+
+api_key = ""
+
+def parse_country_name(country_name):
+    try:
+        return countries.search_fuzzy(country_name)[0].name
+    except LookupError:
+        raise KeyError("Invalid country was specified")
+
+
+def get_distance(fcountry, scountry):
+    resp = requests.post(
+        url="https://distanceto.p.rapidapi.com/distance/route",
+        json={"route":[{"name":fcountry},{"name":scountry}]},
+        headers={"x-rapidapi-key": api_key, "x-rapidapi-host": "distanceto.p.rapidapi.com", "Content-Type": "application/json"}
+    )
+
+    return int(resp.json()['route']['haversine'])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        api_key = sys.argv[1]
+    else:
+        raise TypeError("Missing API Key")
+    
+    fcountry = input("First Country: ")
+    scountry = input("Second Country: ")
+
+    get_distance(parse_country_name(fcountry), parse_country_name(scountry))
